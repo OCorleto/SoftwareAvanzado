a6e5a4c057bf4930c17e7c9be465604e6fda9dda
Oscar Corleto <oscarcorleto000@gmail.com>
2020-08-08 10:34:31 -0600

    Tarea 2 Credentials

--- Practica1/index.js
+++ Practica1/index.js
@@ -4,14 +4,16 @@ function ListaAlumnos(){
     var urlapi = "https://api.softwareavanzado.world/index.php?"
             + "&option=token"
             + "&api=oauth2"
-            + "&grant_type=client_credentials"
-            + "&client_id = sa"
-            + "&client_secret=fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745"
             + "&list[limit]=0"
             + "&filter[search]=201602811"
     $.ajax({
         type:"GET",
         url:urlapi,
+        data : {
+            "&grant_type": "client_credentials",
+            "&client_id" : "sa",
+            "&client_secret" : "fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745"
+        },
         crossDomain:true,
         success : function(data,status){
             var select = document.getElementById('lista');

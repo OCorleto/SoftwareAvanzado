39d9e0dc28f834708883426fed7b17a91733a8dd
Oscar Corleto <oscarcorleto000@gmail.com>
2020-08-14 22:05:26 -0600

    Tarea 2 Parte 2

--- Practica2/index.js
+++ Practica2/index.js
@@ -1,40 +1,56 @@
 
 
 function ListaAlumnos(){
-    var urlapi = "https://api.softwareavanzado.world/index.php?"
-            + "webserviceClient=administrator"
-            + "&webserviceVersion=1.0.0"
-            + "&option=contact"
-            + "&api=hal"
-            + "&list[limit]=0"
-            + "&filter[search]=201602811"
+    var urlsoap="https://api.softwareavanzado.world/index.php?"
+            +"webserviceClient=administrator"
+            +"&webserviceVersion=1.0.0&option=contact"
+            +"&api=soap&wsdl"
+     
     $.ajax({
-        type:"GET",
-        url:urlapi,
+        type:"POST",
+        url:urlsoap,
+        contentType:"text/xml",
+        headers:{'Authorization' : 'Basic sa:usac'},
         crossDomain:true,
+        data : '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:adm="https://api.softwareavanzado.world/media/redcore/webservices/joomla/administrator.contact.1.0.0.wsdl">'
+              +"<soap:Header/>"
+              +"<soap:Body>"
+              +"<adm:readList>"
+              +"<limit>0</limit>"
+              +"<filterSearch>201602811</filterSearch>"
+              +"</adm:readList>"
+              +"</soap:Body>"
+              +"</soap:Envelope>",
         success : function(data,status){
             var select = document.getElementById('lista');
-            console.log(data._embedded.item)
-            data._embedded.item.forEach(function(element) {
+            console.log(data)
+            /*data._embedded.item.forEach(function(element) {
                 select.innerHTML += "<option value='"+element.name+"'>"+element.name+"</option>"; 
                 console.log(element.name)
-              });
+              });*/
         }
     });
 }
 
 function CrearAlumno(){
     var name = document.getElementById('username').value
-    var urlapi ="https://api.softwareavanzado.world/index.php?"
-            + "webserviceClient=administrator"
-            + "&webserviceVersion=1.0.0"
-            + "&option=contact"
-            + "&api=hal"
+    var urlapi ="https://api.softwareavanzado.world/administrator/index.php"
+              +"?webserviceClient=administrator"
+              + "&webserviceVersion=1.0.0"
+              +"&option=contact&api=soap"
     $.ajax({
         type: 'POST',
         url: urlapi,
-        datatype: 'json',
-        data: JSON.stringify({ name : name}),
+        headers:{'Authorization' : 'Basic sa:usac'},
+        contentType : "text/xml",
+        data: '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:adm="https://api.softwareavanzado.world/media/redcore/webservices/joomla/administrator.contact.1.0.0.wsdl">'
+              +"<soap:Header/>"
+              +"<soap:Body>"
+              +   "<adm:create>"
+              +      "<name>"+name+"</name>"
+              +   "</adm:create>"
+              +"</soap:Body>"
+              +"</soap:Envelope>",
         async: true,
         success: function(data){
             console.log(name)
